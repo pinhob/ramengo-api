fda32a56db30cf146e98d48090b4973b1fb00232
pinhob <20286747+pinhob@users.noreply.github.com>
2024-05-30 18:17:01 -0300

    feat: add images to broths and proteins

--- broth/broth.go
+++ broth/broth.go
@@ -10,7 +10,7 @@ type Broth struct {
 }
 
 var BrothsTable = []Broth{
-	{ID: 1, ImageInactive: "#", ImageActive: "#", Name: "Salt", Description: "Simple like the seawater, nothing more", Price: 10},
-	{ID: 2, ImageInactive: "#", ImageActive: "#", Name: "Shoyu", Description: "The good old and traditional soy sauce", Price: 10},
-	{ID: 3, ImageInactive: "#", ImageActive: "#", Name: "Miso", Description: "Paste made of fermented soybeans", Price: 12},
+	{ID: 1, ImageInactive: "https://tech.redventures.com.br/icons/salt/inactive.svg", ImageActive: "https://tech.redventures.com.br/icons/salt/active.svg", Name: "Salt", Description: "Simple like the seawater, nothing more", Price: 10},
+	{ID: 2, ImageInactive: "https://tech.redventures.com.br/icons/shoyu/inactive.svg", ImageActive: "https://tech.redventures.com.br/icons/shoyu/active.svg", Name: "Shoyu", Description: "The good old and traditional soy sauce", Price: 10},
+	{ID: 3, ImageInactive: "https://tech.redventures.com.br/icons/miso/inactive.svg", ImageActive: "https://tech.redventures.com.br/icons/miso/active.svg", Name: "Miso", Description: "Paste made of fermented soybeans", Price: 12},
 }
--- protein/protein.go
+++ protein/protein.go
@@ -10,7 +10,7 @@ type Protein struct {
 }
 
 var ProteinsTable = []Protein{
-	{ID: 1, ImageInactive: "#", ImageActive: "#", Name: "Chasu", Description: "A sliced flavourful pork meat with a selection of season vegetables.", Price: 10},
-	{ID: 2, ImageInactive: "#", ImageActive: "#", Name: "Yasai Vegetarian", Description: "A delicious vegetarian lamen with a selection of season vegetables.", Price: 10},
-	{ID: 3, ImageInactive: "#", ImageActive: "#", Name: "Karaague", Description: "Three units of fried chicken, moyashi, ajitama egg and other vegetables.", Price: 12},
+	{ID: 1, ImageInactive: "https://tech.redventures.com.br/icons/pork/inactive.svg", ImageActive: "https://tech.redventures.com.br/icons/pork/active.svg", Name: "Chasu", Description: "A sliced flavourful pork meat with a selection of season vegetables.", Price: 10},
+	{ID: 2, ImageInactive: "https://tech.redventures.com.br/icons/yasai/inactive.svg", ImageActive: "https://tech.redventures.com.br/icons/yasai/active.svg", Name: "Yasai Vegetarian", Description: "A delicious vegetarian lamen with a selection of season vegetables.", Price: 10},
+	{ID: 3, ImageInactive: "https://tech.redventures.com.br/icons/chicken/inactive.svg", ImageActive: "https://tech.redventures.com.br/icons/chicken/active.svg", Name: "Karaague", Description: "Three units of fried chicken, moyashi, ajitama egg and other vegetables.", Price: 12},
 }
