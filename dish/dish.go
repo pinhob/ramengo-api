f79225d124d6c88850f30e74718bd5319b9b105d
pinhob <20286747+pinhob@users.noreply.github.com>
2024-05-30 19:43:02 -0300

    fix: mock dish table condiments ids

--- dish/dish.go
+++ dish/dish.go
@@ -11,15 +11,15 @@ type Dish struct {
 }
 
 var DishTable = []Dish{
-	{ID: 1, Name: "Shoyu and Chasu Ramen", Image: "#", BrothId: 1, ProteinId: 1},
-	{ID: 2, Name: "Shoyu and Yasai Vegetable Ramen", Image: "#", BrothId: 1, ProteinId: 2},
-	{ID: 3, Name: "Shoyu and Karague Ramen", Image: "#", BrothId: 1, ProteinId: 3},
-	{ID: 4, Name: "Miso and Chasu Ramen", Image: "#", BrothId: 3, ProteinId: 1},
-	{ID: 5, Name: "Miso and Yasai Vegetable Ramen", Image: "#", BrothId: 3, ProteinId: 2},
-	{ID: 6, Name: "Miso and Kaarague Ramen", Image: "#", BrothId: 3, ProteinId: 3},
 	{ID: 7, Name: "Salt and Chasu Ramen", Image: "#", BrothId: 1, ProteinId: 1},
 	{ID: 8, Name: "Salt and Yasai Vegetable Ramen", Image: "#", BrothId: 1, ProteinId: 2},
 	{ID: 9, Name: "Salt and Karaague Ramen", Image: "#", BrothId: 1, ProteinId: 3},
+	{ID: 1, Name: "Shoyu and Chasu Ramen", Image: "#", BrothId: 2, ProteinId: 1},
+	{ID: 2, Name: "Shoyu and Yasai Vegetable Ramen", Image: "#", BrothId: 2, ProteinId: 2},
+	{ID: 3, Name: "Shoyu and Karague Ramen", Image: "#", BrothId: 2, ProteinId: 3},
+	{ID: 4, Name: "Miso and Chasu Ramen", Image: "#", BrothId: 3, ProteinId: 1},
+	{ID: 5, Name: "Miso and Yasai Vegetable Ramen", Image: "#", BrothId: 3, ProteinId: 2},
+	{ID: 6, Name: "Miso and Kaarague Ramen", Image: "#", BrothId: 3, ProteinId: 3},
 }
 
 func GetDish(brothId, proteinId int) (error, Dish) {
