ec60bbb361cfd30b007bbad6c174c71410fe692c
pinhob <20286747+pinhob@users.noreply.github.com>
2024-06-04 21:53:27 -0300

    chore: call http package as internal_http in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,19 +4,20 @@ import (
 	"log"
 	"net/http"
 
-	ihttp "github.com/pinhob/ramengo-api/internal/http"
+	internal_http "github.com/pinhob/ramengo-api/internal/http"
 	"github.com/pinhob/ramengo-api/internal/middlewares"
 )
 
 func main() {
 	router := http.NewServeMux()
+
 	middlewares := middlewares.MiddlewareChain(
 		middlewares.RequestLoggerMiddleware,
 		middlewares.CorsMiddleware,
 		middlewares.ValidateHeaderMiddleware,
 	)
 
-	ihttp.ConfigureServices()
-	ihttp.SetRoutes(router)
+	internal_http.ConfigureServices()
+	internal_http.SetRoutes(router)
 	log.Fatal(http.ListenAndServe(":8080", middlewares(router)))
 }
